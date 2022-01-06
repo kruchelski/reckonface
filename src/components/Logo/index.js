6347d06143e9124250ba80edadb018d1538fecdb
Cassiano Kruchelski Vidal <casst@protonmail.com>
2022-01-05 21:51:31 -0300

    Add logo

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,9 +1,15 @@
 import './style.css';
+import logo from '../../assets/logo.png'
+import Tilt from 'react-parallax-tilt';
 
 function Logo () {
     return(
-        <div>
-            Logo here
+        <div className='logo-container'>
+            <Tilt glareEnable={true} tiltMaxAngleX={15} tiltMaxAngleY={15}>
+                <div className='logo-container__content'>
+                    <img src={logo} alt='Brain image' className='logo-container__image'></img>
+                </div>
+            </Tilt>
         </div>
     )
 }
