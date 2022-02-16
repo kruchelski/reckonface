0388deab76941c48e5c0c52bc512441633f59751
Cassiano Kruchelski Vidal <casst@protonmail.com>
2022-02-15 23:06:50 -0300

    add env example and add front end at env

--- api/src/middlewares/CorsMiddleware.js
+++ api/src/middlewares/CorsMiddleware.js
@@ -1,5 +1,11 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const frontEndUrl = process.env.FRONT_END_URL || 'http://localhost:3000';
+
 const CorsMiddleware = (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', frontEndUrl);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, OPTIONS, HEAD');
   res.setHeader('Access-Control-Allow-Headers', '*');
 
